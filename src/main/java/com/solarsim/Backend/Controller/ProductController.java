00483945d2f9c5b9508c1986930a04e7c6f34c4e
Bruno Winter <brunohdwinter@gmail.com>
2025-05-18 21:23:47 -0400

    Refactor: Organizando sistema de Controller/Facade/Service - Basicamente organizei a responsabilidade de enviar o produto para o seu service correto para um componente propio o ProductServiceFacade para fazer um filtro e funil independente do controller assim mantendo o padrao SOLID

--- src/main/java/com/solarsim/Backend/Controller/ProductController.java
+++ src/main/java/com/solarsim/Backend/Controller/ProductController.java
@@ -1,117 +1,29 @@
 package com.solarsim.Backend.Controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.solarsim.Backend.Model.Product.*;
 import com.solarsim.Backend.Model.Product.ProductDTO.ProductDTO;
 import com.solarsim.Backend.Model.Product.ProductType.ProductType;
-import com.solarsim.Backend.Service.ProductService.*;
+import com.solarsim.Backend.Service.ProductService.ProductServiceFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("product")
 public class ProductController {
 
     @Autowired
-    private CableService cableService;
-    @Autowired
-    private ConectorMc4Service conectorMc4Service;
-    @Autowired
-    private InverterService inverterService;
-    @Autowired
-    private SolarPanelService solarPanelService;
-    @Autowired
-    private SolarPanelSupportService solarPanelSupportService;
+    private ProductServiceFacade productServiceFacade;
 
     @PostMapping
     public ResponseEntity<Void> addProduct(@RequestBody ProductDTO productDTO) {
-        ProductService service = productHandler(productDTO.productType());
-        Product product = convertTo(productDTO.productType(), productDTO.data());
-        service.addProduct(product);
+        productServiceFacade.addProduct(productDTO);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping
     public ResponseEntity<?> getProducts(@RequestParam(required = false) ProductType productType,
-                                         @RequestParam(required = false) String id ) {
-        if (productType != null && id != null) {
-            ProductService service = productHandler(productType);
-            Product product = service.getProductById(id);
-            return ResponseEntity.ok(product);
-        }
-
-        if (productType != null) {
-            ProductService service = productHandler(productType);
-            List<Product> products = service.getAllProducts();
-            return ResponseEntity.ok(products);
-        }
-
-        Map<ProductType, List<Product>> allProducts = new HashMap<>();
-
-        List<Product> cables = cableService.getAllProducts();
-        allProducts.put(ProductType.CABLE, new ArrayList<>(cables));
-
-        List<Product> inverters = inverterService.getAllProducts();
-        allProducts.put(ProductType.INVERTER, new ArrayList<>(inverters));
-
-        List<Product> solarPanels = solarPanelService.getAllProducts();
-        allProducts.put(ProductType.SOLARPANEL, new ArrayList<>(solarPanels));
-
-        List<Product> solarPanelSupports = solarPanelSupportService.getAllProducts();
-        allProducts.put(ProductType.SOLARPANELSUPPORT, new ArrayList<>(solarPanelSupports));
-
-        List<Product> conectors = conectorMc4Service.getAllProducts();
-        allProducts.put(ProductType.CONECTORMC4, new ArrayList<>(conectors));
-
-        return ResponseEntity.ok(allProducts);
-    }
-
-    private ProductService productHandler(ProductType productType) {
-        switch (productType) {
-            case CABLE:
-                return cableService;
-            case CONECTORMC4:
-                return conectorMc4Service;
-            case INVERTER:
-                return inverterService;
-            case SOLARPANEL:
-                return solarPanelService;
-            case SOLARPANELSUPPORT:
-                return solarPanelSupportService;
-            default:
-                throw new IllegalArgumentException("Unknown product type: " + productType);
-        }
-    }
-
-    private Product convertTo(ProductType productType, Map<String, Object> data) {
-        ObjectMapper mapper = new ObjectMapper();
-        Product product = null;
-
-        switch (productType) {
-            case CABLE:
-                product = mapper.convertValue(data, Cable.class);
-                break;
-            case CONECTORMC4:
-                product = mapper.convertValue(data, ConectorMc4.class);
-                break;
-            case INVERTER:
-                product = mapper.convertValue(data, Inverter.class);
-                break;
-            case SOLARPANEL:
-                product = mapper.convertValue(data, SolarPanel.class);
-                break;
-            case SOLARPANELSUPPORT:
-                product = mapper.convertValue(data, SolarPanelSupport.class);
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown product type: " + productType);
-        }
-        return product;
+                                         @RequestParam(required = false) String id) {
+        Object result = productServiceFacade.getProducts(productType, id);
+        return ResponseEntity.ok(result);
     }
 }
